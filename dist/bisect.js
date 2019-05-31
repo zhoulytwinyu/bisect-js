30b8334fd5c625e362b8789ea0a2e0ae993bf435
Lingyu Zhou <zhoulytwin@gmail.com>
2019-05-31 15:14:57 -0400

    switched to babel7

--- dist/bisect.js
+++ dist/bisect.js
@@ -5,6 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.bisect_left = bisect_left;
 exports.bisect_right = bisect_right;
+
 function bisect_left(sorted, target) {
   /*
    * Find the index s.t. sorted[index]<target and sorted[index+1]>=target
@@ -13,14 +14,17 @@ function bisect_left(sorted, target) {
   var l = 0;
   var r = sorted.length - 1;
   var m = null;
+
   while (r >= l) {
     m = ~~((r + l) / 2);
+
     if (sorted[m] >= target) {
       r = m - 1;
     } else {
       l = m + 1;
     }
   }
+
   return r;
 }
 
@@ -32,13 +36,16 @@ function bisect_right(sorted, target) {
   var l = 0;
   var r = sorted.length - 1;
   var m = null;
+
   while (r >= l) {
     m = ~~((r + l) / 2);
+
     if (sorted[m] > target) {
       r = m - 1;
     } else {
       l = m + 1;
     }
   }
+
   return l;
 }
