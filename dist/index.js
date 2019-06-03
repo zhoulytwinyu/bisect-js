ec3f89b48fdf80ae834aa61bc837730ecdce5397
Lingyu Zhou <zhoulytwin@gmail.com>
2019-06-03 11:26:45 -0400

    fix

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "bisect_left", {
+  enumerable: true,
+  get: function get() {
+    return _bisect.bisect_left;
+  }
+});
+Object.defineProperty(exports, "bisect_right", {
+  enumerable: true,
+  get: function get() {
+    return _bisect.bisect_right;
+  }
+});
+
+var _bisect = require("bisect");
