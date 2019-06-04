acd7a1bdc7f67f5f89e72eba3adc06c933bc2ed5
Lingyu Zhou <zhoulytwin@gmail.com>
2019-06-04 10:22:18 -0400

    corrected import bug.

--- dist/index.js
+++ dist/index.js
@@ -3,17 +3,49 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "bisect_left", {
-  enumerable: true,
-  get: function get() {
-    return _bisect.bisect_left;
-  }
-});
-Object.defineProperty(exports, "bisect_right", {
-  enumerable: true,
-  get: function get() {
-    return _bisect.bisect_right;
-  }
-});
+exports.bisect_left = bisect_left;
+exports.bisect_right = bisect_right;
 
-var _bisect = require("bisect");
+function bisect_left(sorted, target) {
+  /*
+   * Find the index s.t. sorted[index]<target and sorted[index+1]>=target
+   * ([1,2,3],2) returns 0
+   */
+  var l = 0;
+  var r = sorted.length - 1;
+  var m = null;
+
+  while (r >= l) {
+    m = ~~((r + l) / 2);
+
+    if (sorted[m] >= target) {
+      r = m - 1;
+    } else {
+      l = m + 1;
+    }
+  }
+
+  return r;
+}
+
+function bisect_right(sorted, target) {
+  /*
+   * Find the index s.t. sorted[index]>target and sorted[index-1]<=target
+   * ([1,2,3],2) returns 0
+   */
+  var l = 0;
+  var r = sorted.length - 1;
+  var m = null;
+
+  while (r >= l) {
+    m = ~~((r + l) / 2);
+
+    if (sorted[m] > target) {
+      r = m - 1;
+    } else {
+      l = m + 1;
+    }
+  }
+
+  return l;
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-import {bisect_left,bisect_right} from "./bisect";
+export function bisect_left(sorted,target) {
+  /*
+   * Find the index s.t. sorted[index]<target and sorted[index+1]>=target
+   * ([1,2,3],2) returns 0
+   */
+  let l = 0;
+  let r = sorted.length-1;
+  let m = null;
+  while (r>=l) {
+    m = ~~((r+l)/2);
+    if (sorted[m] >= target) {
+      r=m-1;
+    }
+    else {
+      l=m+1;
+    }
+  }
+  return r;
+}
 
-export {bisect_left,bisect_right};
+export function bisect_right(sorted,target) {
+  /*
+   * Find the index s.t. sorted[index]>target and sorted[index-1]<=target
+   * ([1,2,3],2) returns 0
+   */
+  let l = 0;
+  let r = sorted.length-1;
+  let m = null;
+  while (r>=l) {
+    m = ~~((r+l)/2);
+    if (sorted[m] > target) {
+      r=m-1;
+    }
+    else {
+      l=m+1;
+    }
+  }
+  return l;
+}
