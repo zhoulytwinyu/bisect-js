12e06544af22ee5c6eb29643a4e5e86ace5ce07d
Lingyu Zhou <zhoulytwin@gmail.com>
2019-08-06 17:16:30 -0400

    extraction fucntion option added to bisect functions

--- dist/index.js
+++ dist/index.js
@@ -7,45 +7,91 @@ exports.bisect_left = bisect_left;
 exports.bisect_right = bisect_right;
 
 function bisect_left(sorted, target) {
+  var key = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+
   /*
    * Find the index s.t. sorted[index]<target and sorted[index+1]>=target
    * ([1,2,3],2) returns 0
+   * key should be a function that extract the value from each element in the sorted array.
    */
-  var l = 0;
-  var r = sorted.length - 1;
-  var m = null;
+  if (key === null) {
+    var l = 0;
+    var r = sorted.length - 1;
+    var m = null;
 
-  while (r >= l) {
-    m = ~~((r + l) / 2);
+    while (r >= l) {
+      m = ~~((r + l) / 2);
 
-    if (sorted[m] >= target) {
-      r = m - 1;
-    } else {
-      l = m + 1;
+      if (sorted[m] >= target) {
+        r = m - 1;
+      } else {
+        l = m + 1;
+      }
     }
-  }
 
-  return r;
+    return r;
+  } else {
+    var _l = 0;
+
+    var _r = sorted.length - 1;
+
+    var _m = null;
+
+    while (_r >= _l) {
+      _m = ~~((_r + _l) / 2);
+
+      if (key(sorted[_m]) >= target) {
+        _r = _m - 1;
+      } else {
+        _l = _m + 1;
+      }
+    }
+
+    return _r;
+  }
 }
 
 function bisect_right(sorted, target) {
+  var key = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+
   /*
    * Find the index s.t. sorted[index]>target and sorted[index-1]<=target
    * ([1,2,3],2) returns 0
+   * key should be a function that extract the value from each element in the sorted array.
    */
-  var l = 0;
-  var r = sorted.length - 1;
-  var m = null;
+  if (key === null) {
+    var l = 0;
+    var r = sorted.length - 1;
+    var m = null;
 
-  while (r >= l) {
-    m = ~~((r + l) / 2);
+    while (r >= l) {
+      m = ~~((r + l) / 2);
 
-    if (sorted[m] > target) {
-      r = m - 1;
-    } else {
-      l = m + 1;
+      if (sorted[m] > target) {
+        r = m - 1;
+      } else {
+        l = m + 1;
+      }
     }
-  }
 
-  return l;
+    return l;
+  } else {
+    var _l2 = 0;
+
+    var _r2 = sorted.length - 1;
+
+    var _m2 = null;
+
+    while (_r2 >= _l2) {
+      _m2 = ~~((_r2 + _l2) / 2);
+
+      if (key(sorted[_m2]) > target) {
+        _r2 = _m2 - 1;
+      } else {
+        _l2 = _m2 + 1;
+      }
+    }
+
+    return _l2;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,39 +1,75 @@
-export function bisect_left(sorted,target) {
+export function bisect_left(sorted,target,key=null) {
   /*
    * Find the index s.t. sorted[index]<target and sorted[index+1]>=target
    * ([1,2,3],2) returns 0
+   * key should be a function that extract the value from each element in the sorted array.
    */
-  let l = 0;
-  let r = sorted.length-1;
-  let m = null;
-  while (r>=l) {
-    m = ~~((r+l)/2);
-    if (sorted[m] >= target) {
-      r=m-1;
+  if (key===null) {
+    let l = 0;
+    let r = sorted.length-1;
+    let m = null;
+    while (r>=l) {
+      m = ~~((r+l)/2);
+      if (sorted[m] >= target) {
+        r=m-1;
+      }
+      else {
+        l=m+1;
+      }
     }
-    else {
-      l=m+1;
-    }
+    return r;
   }
-  return r;
+  else {
+    let l = 0;
+    let r = sorted.length-1;
+    let m = null;
+    while (r>=l) {
+      m = ~~((r+l)/2);
+      if (key(sorted[m]) >= target) {
+        r=m-1;
+      }
+      else {
+        l=m+1;
+      }
+    }
+    return r;
+  }
 }
 
-export function bisect_right(sorted,target) {
+export function bisect_right(sorted,target,key=null) {
   /*
    * Find the index s.t. sorted[index]>target and sorted[index-1]<=target
    * ([1,2,3],2) returns 0
+   * key should be a function that extract the value from each element in the sorted array.
    */
-  let l = 0;
-  let r = sorted.length-1;
-  let m = null;
-  while (r>=l) {
-    m = ~~((r+l)/2);
-    if (sorted[m] > target) {
-      r=m-1;
+  if (key===null) {
+    let l = 0;
+    let r = sorted.length-1;
+    let m = null;
+    while (r>=l) {
+      m = ~~((r+l)/2);
+      if (sorted[m] > target) {
+        r=m-1;
+      }
+      else {
+        l=m+1;
+      }
     }
-    else {
-      l=m+1;
-    }
+    return l;
   }
-  return l;
+  else {
+    let l = 0;
+    let r = sorted.length-1;
+    let m = null;
+    while (r>=l) {
+      m = ~~((r+l)/2);
+      if (key(sorted[m]) > target) {
+        r=m-1;
+      }
+      else {
+        l=m+1;
+      }
+    }
+    return l;
+  }
 }
